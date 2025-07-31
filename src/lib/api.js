15a6fe810f6e10ddf1f1dabe0fc1f869c1d41b76
LAPTOP-92NBAG1S\lenovo <nomai6789@gmail.com>
2025-07-31 19:49:13 +0700

    initial setup

---
+++ src/lib/api.js
@@ -0,0 +1,3 @@
+export const API_DOMAIN = import.meta.env.VITE_API_DOMAIN
+export const API_BASE_ADMIN = import.meta.env.VITE_API_BASE_ADMIN
+export const API_BASE_AUTH_PUBLIC = import.meta.env.VITE_API_BASE_AUTH_PUBLIC
