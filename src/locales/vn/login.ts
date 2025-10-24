d000fff0401b97dc7aa7c68a9a05b8f557e63255
LE VU PHONG <lephong29092909@gmail.com>
2025-10-25 01:23:45 +0700

    refactor: clean key translate not use in coupon

--- src/locales/vn/login.ts
+++ src/locales/vn/login.ts
@@ -17,6 +17,7 @@ const login = {
     "Tài khoản của bạn chưa được kích hoạt. Vui lòng kiểm tra email để nhận liên kết kích hoạt.",
   INVALID_CREDENTIALS: "Email hoặc mật khẩu không hợp lệ. Vui lòng thử lại.",
   LOGIN_FORGOT_PASSWORD: "Quên mật khẩu?",
+  FORGOT_PASSWORD_RESENT: "Liên kết kích hoạt mới đã được gửi đến email của bạn.",
 };
 
 export default login;
