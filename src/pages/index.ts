8d2055086e86fa0e0009da3c442f4699056fa9a4
LE VU PHONG <lephong29092909@gmail.com>
2025-10-24 18:38:28 +0700

    refactor

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,7 @@
-export { default as Dashboard } from "./dashboard/dashboard/Dashboard";
+export { default as Dashboard } from "./dashboard/Dashboard";
 export { default as Login } from "./login/Login";
-export { default as RevenueByCinema } from "./dashboard/revenue-cinema/RevenueByCinema";
-export { default as RevenueByMovie } from "./dashboard/revenue-movie/RevenueByMovie";
+export { default as RevenueByCinema } from "./report/revenue-cinema/RevenueByCinema";
+export { default as RevenueByMovie } from "./report/revenue-movie/RevenueByMovie";
 
 // Product pages
 export { default as ProductList } from "./product/product-list/ProductList";
